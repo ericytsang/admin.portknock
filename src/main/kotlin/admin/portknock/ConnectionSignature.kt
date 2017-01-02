c876ecc8c995eab46d84151f6b74930af55b7528
Eric Tsang <surplus.et@gmail.com>
2017-01-02 05:27:42 -0800

    added a pseudo set for the connection signature to improve time and space efficiency

--- src/main/kotlin/admin/portknock/ConnectionSignature.kt
+++ src/main/kotlin/admin/portknock/ConnectionSignature.kt
@@ -13,17 +13,55 @@ data class ConnectionSignature private constructor(val remoteIpAddress:InetAddre
 
         fun createSet(remoteIpAddress:InetAddress,remotePorts:IntRange,localPort:Int):Set<ConnectionSignature>
         {
-            return remotePorts
-                .map {
-                    remotePort ->
-                    createObject(remoteIpAddress,remotePort,localPort)
-                }
-                .toSet()
+            return PseudoSet(remoteIpAddress,remotePorts,localPort)
         }
 
         fun createSet(remoteIpAddress:InetAddress,remotePort:Int,localPort:Int):Set<ConnectionSignature>
         {
             return setOf(createObject(remoteIpAddress,remotePort,localPort))
+        }
+    }
+
+    private class PseudoSet(val remoteIpAddress:InetAddress,val remotePorts:IntRange,val localPort:Int):Set<ConnectionSignature>
+    {
+        override val size:Int get() = remotePorts.last-remotePorts.first+1
+
+        override fun contains(element:ConnectionSignature):Boolean
+        {
+            return element.remoteIpAddress == remoteIpAddress
+                && element.remotePort in remotePorts
+                && element.localPort == localPort
+        }
+
+        override fun containsAll(elements:Collection<ConnectionSignature>):Boolean
+        {
+            return elements.all {contains(it)}
+        }
+
+        override fun isEmpty():Boolean
+        {
+            return size == 0
+        }
+
+        override fun iterator():Iterator<ConnectionSignature>
+        {
+            return CustomIterator()
+        }
+
+        private inner class CustomIterator:AbstractIterator<ConnectionSignature>()
+        {
+            val remotePortsIterator = remotePorts.iterator()
+            override fun computeNext()
+            {
+                if (remotePortsIterator.hasNext())
+                {
+                    setNext(createObject(remoteIpAddress,remotePortsIterator.next(),localPort))
+                }
+                else
+                {
+                    done()
+                }
+            }
         }
     }
 }

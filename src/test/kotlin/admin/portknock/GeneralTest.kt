24aa66780131bd7667c7fdc1bbe63c505aca1047
Eric Tsang <surplus.et@gmail.com>
2017-01-01 17:16:05 -0800

    split test into server-side and client-side tests

--- src/test/kotlin/admin/portknock/GeneralTest.kt
+++ src/test/kotlin/admin/portknock/GeneralTest.kt
@@ -9,9 +9,28 @@ import java.util.ArrayList
 
 class GeneralTest
 {
-    val clientKeyPair = generateKeyPair(2048)
+    companion object
+    {
+        val CLIENT_KEY_PAIR = generateKeyPair(2048)
 
-    val serverKeyPair = generateKeyPair(4096)
+        val SERVER_KEY_PAIR = generateKeyPair(4096)
+
+        const val KNOCK_PORT = 51268
+
+        const val CONTROL_PORT = 62513
+
+        private fun generateKeyPair(keySize:Int):KeyPair
+        {
+            val keyGen = KeyPairGenerator.getInstance("RSA")
+            keyGen.initialize(keySize)
+            val keyPair = keyGen.generateKeyPair()
+            println("encrypt key: ${keyPair.public.encoded.toHexString()}")
+            println("decrypt key: ${keyPair.private.encoded.toHexString()}")
+            return keyPair
+        }
+    }
+
+    val serverAddress = InetAddress.getByName("ennui")
 
     val printFirewall = object:Firewall
     {
@@ -31,7 +50,7 @@ class GeneralTest
         override fun get(publicKey:List<Byte>):ClientInfo?
         {
             println("printPersister[${publicKey.hashCode()}]")
-            return ClientInfo(100,clientKeyPair.public.encoded.mapTo(ArrayList()){it},"custom client")
+            return ClientInfo(100,CLIENT_KEY_PAIR.public.encoded.mapTo(ArrayList()){it},"custom client")
         }
 
         override fun set(publicKey:List<Byte>,client:ClientInfo)
@@ -43,31 +62,26 @@ class GeneralTest
             get() = throw UnsupportedOperationException() // todo
     }
 
-    val server = PortKnockServer(printPersister,printFirewall,serverKeyPair,51268,62513)
+    @Test
+    fun server()
+    {
+        val server = PortKnockServer(printPersister,printFirewall,SERVER_KEY_PAIR,KNOCK_PORT,CONTROL_PORT)
+        sleep(120000)
+    }
 
     @Test
-    fun doPortKnockAndRequestTcpPortOpen()
+    fun client()
     {
         sleep(1000)
         val session = PortKnockClient.connect(
             {println(it)},
-            ServerInfo(100,"hellur",InetAddress.getLocalHost(),
-                serverKeyPair.public.encoded.mapTo(ArrayList()){it},51268,
-                62513),
-            clientKeyPair)
+            ServerInfo(100,"hellur",serverAddress,
+                SERVER_KEY_PAIR.public.encoded.mapTo(ArrayList()){it},
+                KNOCK_PORT,CONTROL_PORT),
+            CLIENT_KEY_PAIR)
         val closeable = session.requestTcpConnectClearance(22)
         sleep(5000)
         closeable.close()
         session.close()
-    }
-
-    fun generateKeyPair(keySize:Int):KeyPair
-    {
-        val keyGen = KeyPairGenerator.getInstance("RSA")
-        keyGen.initialize(keySize)
-        val keyPair = keyGen.generateKeyPair()
-        println("encrypt key: ${keyPair.public.encoded.toHexString()}")
-        println("decrypt key: ${keyPair.private.encoded.toHexString()}")
-        return keyPair
     }
 }

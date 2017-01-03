8609d717a2ab88842b2b47c2a04ef27b0092b2f9
Eric Tsang <surplus.et@gmail.com>
2017-01-03 07:17:00 -0800

    moved static function declarations in the main module that were only used in the test module into the test module
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/main/kotlin/admin/portknock/StaticFuns.kt
+++ src/main/kotlin/admin/portknock/StaticFuns.kt
@@ -2,37 +2,8 @@ package admin.portknock
 
 import org.pcap4j.core.PcapHandle
 import org.pcap4j.core.PcapNetworkInterface
-import java.security.KeyFactory
-import java.security.PrivateKey
-import java.security.PublicKey
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.spec.X509EncodedKeySpec
-
-private val hexArray = "0123456789ABCDEF".toCharArray()
-
-internal fun ByteArray.toHexString():String
-{
-    val hexChars = CharArray(size*2)
-    for (j in 0..size-1)
-    {
-        val v = this[j].toInt() and 0xFF
-        hexChars[j*2] = hexArray[v.ushr(4)]
-        hexChars[j*2+1] = hexArray[v and 0x0F]
-    }
-    return String(hexChars)
-}
 
 internal fun PcapNetworkInterface.open():PcapHandle
 {
     return openLive(65536,PcapNetworkInterface.PromiscuousMode.NONPROMISCUOUS,5000)
-}
-
-internal fun ByteArray.toRsaPrivateKey():PrivateKey
-{
-    return KeyFactory.getInstance("RSA").generatePrivate(PKCS8EncodedKeySpec(this))
-}
-
-internal fun ByteArray.toRsaPublicKey():PublicKey
-{
-    return KeyFactory.getInstance("RSA").generatePublic(X509EncodedKeySpec(this))
 }
---
+++ src/test/kotlin/admin/portknock/MoreStaticFuns.kt
@@ -0,0 +1,31 @@
+package admin.portknock
+
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+
+private val hexArray = "0123456789ABCDEF".toCharArray()
+
+internal fun ByteArray.toHexString():String
+{
+    val hexChars = CharArray(size*2)
+    for (j in 0..size-1)
+    {
+        val v = this[j].toInt() and 0xFF
+        hexChars[j*2] = hexArray[v.ushr(4)]
+        hexChars[j*2+1] = hexArray[v and 0x0F]
+    }
+    return String(hexChars)
+}
+
+internal fun ByteArray.toRsaPrivateKey():PrivateKey
+{
+    return KeyFactory.getInstance("RSA").generatePrivate(PKCS8EncodedKeySpec(this))
+}
+
+internal fun ByteArray.toRsaPublicKey():PublicKey
+{
+    return KeyFactory.getInstance("RSA").generatePublic(X509EncodedKeySpec(this))
+}
